cb11a528a63e8efb31c1647af73e7b37acca81a1
Sahasil AK <76594303+SahasilAK@users.noreply.github.com>
2021-02-04 14:04:44 +0530

    Add files via upload

---
+++ custom_msg.py
@@ -0,0 +1,18 @@
+hi_lists = [
+    'എന്നെ വിളിച്ചോ ', 'നിർത്തീട് വല്ല കൂലിപ്പണിക്കും പോ',
+    'sry എനിക്ക് ജാഡ ആണ്', 'Sup', 'Hi', 'Hello', 'നമസ്ക്കാരം',
+    ' عَلَيْكُمْ اَلسَّلَامُُ', 'Eat\nSleep\nPlay\nRepeat',
+    'valorant skins are trash\n**Change My Mind**', 'Busy playing Pubg Mobile',
+    'Busy playing Freefire', 'What’s shakin’?', 'Doing OK?', 'Bored',
+    'Have a Great Game Day','I Missed you','Omg ur responses are so fast. Slow down. I cant keep up','||KSEB ഉയിർ||'
+]
+colors_list = [
+    0x1abc9c, 0x11806a, 0x2ecc71, 0x1f8b4c, 0x3498db, 0x206694, 0x9b59b6,
+    0x71368a, 0xe91e63, 0xad1457, 0xf1c40f, 0xc27c0e, 0xe67e22, 0xa84300,
+    0xe74c3c, 0x992d22
+]
+bot_commands = [
+    '.hi', '.yt or .youtube', '.discord or .sendlink', '.roles', '.support',
+    '.admin'
+]
+hidden_commands = ['.a10', '.hentai']
---
+++ keep_alive.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, render_template
+from threading import Thread
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
+import os
+from flask_login import current_user
+import json
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label='Username', validators=[DataRequired()])
+    password = PasswordField(label='Password',
+                             validators=[DataRequired(),
+                                         Length(min=8)])
+    submit = SubmitField(label='Log In')
+
+
+class AnnounceForm(FlaskForm):
+    title = StringField(label="Title", validators=[DataRequired()])
+    desc = TextAreaField(label="Description")
+    link = StringField(label="Url")
+    image_link = StringField(label="Url for Image")
+    a_submit = SubmitField(label='Announce')
+
+
+app = Flask('')
+app.secret_key = os.getenv('SECRET_KEY')
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    login_form = LoginForm()
+
+    if login_form.validate_on_submit():
+        if login_form.username.data == os.getenv(
+                'USERNAME') and login_form.password.data == os.getenv(
+                    'PASSWORD'):
+            return render_template('success.html',form={'done':'What are you going to Announce today'})
+        else:
+            return render_template('denied.html', cure)
+
+    return render_template('login.html', form=login_form)
+
+
+@app.route("/announce", methods=["POST", "GET"])
+def announce():
+    announce_form = AnnounceForm()
+    if announce_form.validate_on_submit():
+        a_title = announce_form.title.data
+        a_desc = announce_form.desc.data
+        a_link = announce_form.link.data
+        a_img = announce_form.image_link.data
+        update_anounce(a_title, a_desc, a_link, a_img)
+
+        return render_template('success.html', form={'done':'Announced'})
+
+    return render_template('announce.html', form=announce_form)
+
+
+def update_anounce(title, desc, link, img):
+    announce_data = {
+        'title': title,
+        'description': desc,
+        'url': link,
+        'img_url': img
+    }
+    with open('data.json','w') as data_file:
+      json.dump(announce_data,data_file)
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
